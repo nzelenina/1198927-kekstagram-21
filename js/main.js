3f68d8fe7e7796db39fbdde12ab4fd5992dd5793
nataliya <n.zelenina1967@gmail.com>
2020-09-28 11:47:55 +0300

    фрагмент

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-let messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-let names = ['Артем', 'Марья', 'Василий', 'Савелий', 'Валентина', 'Игорь'];
-let comments = [];
-let photos = [];
+var messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var names = ['Артем', 'Марья', 'Василий', 'Савелий', 'Валентина', 'Игорь'];
+var comments = [];
+var photos = [];
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -11,8 +11,8 @@ function getRandomArrayElement(array) {
   return array[getRndInteger(0, array.length - 1)];
 }
 
-for (let i = 0; i <= getRndInteger(1, 4); i++) {
-  let comment = {
+for (var i = 0; i <= getRndInteger(1, 4); i++) {
+  var comment = {
     avatar: 'img/avatar-' + getRndInteger(1, 6) + '.svg',
     message: getRandomArrayElement(messages),
     name: getRandomArrayElement(names)
@@ -20,7 +20,7 @@ for (let i = 0; i <= getRndInteger(1, 4); i++) {
   comments.push(comment)
 }
 
-for (let i = 0; i < 25; i++) {
+for (var i = 0; i < 25; i++) {
   var object = {
     url: 'photos/' + i + '.jpg',
     likes: getRndInteger(15, 200),
@@ -30,15 +30,21 @@ for (let i = 0; i < 25; i++) {
   photos.push(object)
 }
 
-let allUserPicture = document.querySelector('.pictures');
-let userPictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+var allUserPicture = document.querySelector('.pictures');
+var userPictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-for (i = 1; i < photos.length; i++) {
-  let userPicture = userPictureTemplate.cloneNode(true);
-  let pictureImg = userPicture.querySelector('.picture__img');
-  let image = photos[i].url;
+var getUserPicture = function(photos) {
+  var userPicture = userPictureTemplate.cloneNode(true);
+  var pictureImg = userPicture.querySelector('.picture__img');
+  var image = photos.url;
   pictureImg.setAttribute('src', image);
-  userPicture.querySelector('.picture__comments').textContent = photos[i].comments;
-  userPicture.querySelector('.picture__likes').textContent = photos[i].likes;
-  allUserPicture.appendChild(userPicture);
+  userPicture.querySelector('.picture__comments').textContent = photos.comments;
+  userPicture.querySelector('.picture__likes').textContent = photos.likes;
+
+ return userPicture;
 }
+var fragment = document.createDocumentFragment();
+for ( i = 1; i < photos.length; i++) {
+  fragment.appendChild(getUserPicture(photos[i]));
+}
+  allUserPicture.appendChild(fragment);
